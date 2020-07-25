5a71a08b4b0536c413a61903d705156121a10014
Zhong Ruoyu <zhongruoyu@outlook.com>
2020-07-25 11:04:19 +0800

    Script updated

--- scripts/ntu_matric.js
+++ scripts/ntu_matric.js
@@ -2,12 +2,26 @@ function validate() {
     function checkdigit(type, year, num) {
         var WEIGHT = [0, 0, 0, 0, 0, 0, 0];
         var OFFSET = 0;
-        var RESULTS = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "ERROR"];
+        var RESULTS = [
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "J",
+            "K",
+            "L",
+            "ERROR",
+        ];
         var sum = 0;
         var remainder = 11;
         switch (type) {
             case "U":
-                if (year >= 17) { // *
+                if (year >= 17) {
+                    // *
                     WEIGHT = [6, 7, 4, 3, 8, 9, 2];
                     OFFSET = 4;
                 } else {
@@ -51,7 +65,8 @@ function validate() {
         type = input.value.substring(0, 1).toUpperCase();
         year = parseInt(input.value.substring(1, 3));
         num = parseInt(input.value.substring(1, 8));
-        result.innerHTML = "The check digit is " + checkdigit(type, year, num) + ".";
+        result.innerHTML =
+            "The check digit is " + checkdigit(type, year, num) + ".";
         return;
     }
     if (input.value.match(/^[A-Z]\d{7}[ABCDEFGHJKL]/i)) {
@@ -60,14 +75,19 @@ function validate() {
         num = parseInt(input.value.substring(1, 8));
         check = input.value.substring(8, 9).toUpperCase();
         if (check == checkdigit(type, year, num)) {
-            result.innerHTML = input.value.toUpperCase() + " is a valid matric number.";
+            result.innerHTML =
+                input.value.toUpperCase() + " is a valid matric number.";
         } else {
-            result.innerHTML = input.value.toUpperCase() + " is not a valid matric number. Please check your input.";
+            result.innerHTML =
+                input.value.toUpperCase() +
+                " is not a valid matric number. Please check your input.";
         }
         return;
     }
     if (input.value.match(/^[A-Z]\d{7}[A-Z]/i)) {
-        result.innerHTML = input.value.toUpperCase() + " is not a valid matric number. Please check your input.";
+        result.innerHTML =
+            input.value.toUpperCase() +
+            " is not a valid matric number. Please check your input.";
         return;
     }
     result.innerHTML = "Invalid input.";
